9a26c26257fda5280271140ccbe2ee4d48923dc7
cardavid11 <34225554+cardavid11@users.noreply.github.com>
2019-06-20 13:22:19 -0400

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '315875685969253',
+        clientSecret: '30dd78a202c97375dbc1da5de94637af'
+    }
 }
